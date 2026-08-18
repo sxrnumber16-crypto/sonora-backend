d681617296be4cf295af60b9767d870ec37c0387
sxrnumber16-crypto <sxrnumber16-crypto@users.noreply.github.com>
2026-08-19 03:57:27 +0600

    Set default fallback port 8080 for web-production-e4682.up.railway.app

--- main.py
+++ main.py
@@ -346,5 +346,5 @@ async def download_audio_stream(video_id: str, req: Request):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
